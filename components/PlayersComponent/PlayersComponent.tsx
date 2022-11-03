3751621e0ea26cdf903a3c3a34f200b0547b1568
vujadinjanjic <skakavac49@gmail.com>
2022-11-03 11:19:51 +0100

    Add routing on player

--- components/PlayersComponent/PlayersComponent.tsx
+++ components/PlayersComponent/PlayersComponent.tsx
@@ -2,6 +2,7 @@ import styles from "./PlayersComponent.module.scss";
 import Image from "next/image";
 import player from "../../images/batum.svg";
 import director from "../../images/director.svg";
+import { useRouter } from "next/router";
 
 const players = [
   {
@@ -39,12 +40,18 @@ const players = [
 ];
 
 const PlayersComponent = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <span className={styles.title}>Lorem ipsum</span>
       <div className={styles.playerContainer}>
         {players.map((player) => (
-          <div className={styles.player} key={player.name}>
+          <div
+            className={styles.player}
+            key={player.name}
+            onClick={() => router.push("/player")}
+          >
             <Image alt="" src={player.img} className={styles.img} />
             <span>{player.name}</span>
           </div>
